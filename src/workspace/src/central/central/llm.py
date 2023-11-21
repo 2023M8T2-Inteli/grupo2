405f3685c3081254adea160e87996850c12bc456
Lyorrei Shono Quintão <lyorrei.quintao@sou.inteli.edu.br>
2023-11-21 17:08:25 -0300

    Fixed LLM node not returning response

--- src/workspace/src/central/central/llm.py
+++ src/workspace/src/central/central/llm.py
@@ -2,6 +2,8 @@ from rclpy.node import Node
 from std_msgs.msg import String
 import rclpy
 from langchain.llms import Ollama
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableMap, RunnablePassthrough
 
 class LlmNode(Node):
     def __init__(self, base_url, model_name):
@@ -9,17 +11,20 @@ class LlmNode(Node):
         self.subscription = self.create_subscription(
             String, "llm_command", self.listener_callback, 10
         )
-        self.publisher_ = self.create_publisher(String, 'llm_response', 10)
+        self.publisher_ = self.create_publisher(String, "llm_response", 10)
         self.ollama = Ollama(base_url=base_url, model=model_name)
         self.get_logger().info("LLM Node está rodando e esperando por comandos...")
 
     def run_ollama(self, text):
         try:
-          ollama_response = self.ollama(text)
-          return ollama_response
+            ollama_response = ""
+            for s in self.ollama.stream(text):
+                ollama_response += s
+
+            return ollama_response
         except Exception as exp:
-          self.get_logger().info(exp)
-            
+            self.get_logger().info(exp)
+
     def listener_callback(self, msg):
         self.get_logger().info(f'LLM recebeu: "{msg.data}"')
         response = self.run_ollama(msg.data)
@@ -29,9 +34,13 @@ class LlmNode(Node):
         response_msg.data = response
         self.publisher_.publish(response_msg)
 
+
 def main(args=None):
     rclpy.init(args=args)
-    llm_node = LlmNode(base_url='http://localhost:11434', model_name="dolphin2.2-mistral")
+    llm_node = LlmNode(
+        base_url="http://localhost:11434",
+        model_name="dolphin2.2-mistral",
+    )
 
     try:
         rclpy.spin(llm_node)
@@ -40,6 +49,7 @@ def main(args=None):
     finally:
         llm_node.destroy_node()
         rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
