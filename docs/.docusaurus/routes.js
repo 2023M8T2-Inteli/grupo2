d2b26414813614dc10669a005cdffa2652d5099b
edu-porto <99202389+edu-porto@users.noreply.github.com>
2023-12-22 14:02:44 -0300

    restante json

---
+++ docs/.docusaurus/routes.js
@@ -0,0 +1,452 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/grupo2/__docusaurus/debug',
+    component: ComponentCreator('/grupo2/__docusaurus/debug', '78b'),
+    exact: true
+  },
+  {
+    path: '/grupo2/__docusaurus/debug/config',
+    component: ComponentCreator('/grupo2/__docusaurus/debug/config', '939'),
+    exact: true
+  },
+  {
+    path: '/grupo2/__docusaurus/debug/content',
+    component: ComponentCreator('/grupo2/__docusaurus/debug/content', 'b67'),
+    exact: true
+  },
+  {
+    path: '/grupo2/__docusaurus/debug/globalData',
+    component: ComponentCreator('/grupo2/__docusaurus/debug/globalData', '30b'),
+    exact: true
+  },
+  {
+    path: '/grupo2/__docusaurus/debug/metadata',
+    component: ComponentCreator('/grupo2/__docusaurus/debug/metadata', 'd83'),
+    exact: true
+  },
+  {
+    path: '/grupo2/__docusaurus/debug/registry',
+    component: ComponentCreator('/grupo2/__docusaurus/debug/registry', '3b3'),
+    exact: true
+  },
+  {
+    path: '/grupo2/__docusaurus/debug/routes',
+    component: ComponentCreator('/grupo2/__docusaurus/debug/routes', '888'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog',
+    component: ComponentCreator('/grupo2/blog', '2c8'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/archive',
+    component: ComponentCreator('/grupo2/blog/archive', '251'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/first-blog-post',
+    component: ComponentCreator('/grupo2/blog/first-blog-post', '427'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/long-blog-post',
+    component: ComponentCreator('/grupo2/blog/long-blog-post', '124'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/mdx-blog-post',
+    component: ComponentCreator('/grupo2/blog/mdx-blog-post', '4d8'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/tags',
+    component: ComponentCreator('/grupo2/blog/tags', '151'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/tags/docusaurus',
+    component: ComponentCreator('/grupo2/blog/tags/docusaurus', 'bea'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/tags/facebook',
+    component: ComponentCreator('/grupo2/blog/tags/facebook', '3a6'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/tags/hello',
+    component: ComponentCreator('/grupo2/blog/tags/hello', '2c5'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/tags/hola',
+    component: ComponentCreator('/grupo2/blog/tags/hola', '207'),
+    exact: true
+  },
+  {
+    path: '/grupo2/blog/welcome',
+    component: ComponentCreator('/grupo2/blog/welcome', '077'),
+    exact: true
+  },
+  {
+    path: '/grupo2/markdown-page',
+    component: ComponentCreator('/grupo2/markdown-page', 'a89'),
+    exact: true
+  },
+  {
+    path: '/grupo2/',
+    component: ComponentCreator('/grupo2/', '3ee'),
+    routes: [
+      {
+        path: '/grupo2/',
+        component: ComponentCreator('/grupo2/', 'd2c'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/category/entendimento-do-negócio',
+        component: ComponentCreator('/grupo2/category/entendimento-do-negócio', '9bd'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/category/entendimento-do-negócio-1',
+        component: ComponentCreator('/grupo2/category/entendimento-do-negócio-1', '3a7'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/category/sprint-1',
+        component: ComponentCreator('/grupo2/category/sprint-1', '622'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/category/sprint-2',
+        component: ComponentCreator('/grupo2/category/sprint-2', 'aed'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/category/sprint-3',
+        component: ComponentCreator('/grupo2/category/sprint-3', '6b3'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/category/sprint-4',
+        component: ComponentCreator('/grupo2/category/sprint-4', 'c72'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/category/sprint-5',
+        component: ComponentCreator('/grupo2/category/sprint-5', 'c7c'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Análise de impacto ético/',
+        component: ComponentCreator('/grupo2/sprint1/Análise de impacto ético/', 'c4c'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Arquitetura do sistema/',
+        component: ComponentCreator('/grupo2/sprint1/Arquitetura do sistema/', '738'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Banco de dados/',
+        component: ComponentCreator('/grupo2/sprint1/Banco de dados/', 'aed'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Design Patterns/',
+        component: ComponentCreator('/grupo2/sprint1/Design Patterns/', '0d7'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Entendimento da experiência do usuário/',
+        component: ComponentCreator('/grupo2/sprint1/Entendimento da experiência do usuário/', 'd13'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Entendimento da experiência do usuário/Jornada do Usuario/',
+        component: ComponentCreator('/grupo2/sprint1/Entendimento da experiência do usuário/Jornada do Usuario/', 'ea8'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Entendimento da experiência do usuário/Personas/',
+        component: ComponentCreator('/grupo2/sprint1/Entendimento da experiência do usuário/Personas/', 'fcc'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Entendimento da experiência do usuário/User Stories/',
+        component: ComponentCreator('/grupo2/sprint1/Entendimento da experiência do usuário/User Stories/', 'bf3'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Entendimento do negócio/Análise Financeira/',
+        component: ComponentCreator('/grupo2/sprint1/Entendimento do negócio/Análise Financeira/', 'b41'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Entendimento do negócio/Canvas Proposta de Valor/',
+        component: ComponentCreator('/grupo2/sprint1/Entendimento do negócio/Canvas Proposta de Valor/', '76b'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Entendimento do negócio/Matriz de Risco/',
+        component: ComponentCreator('/grupo2/sprint1/Entendimento do negócio/Matriz de Risco/', 'a35'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Entendimento do negócio/Matriz Oceano Aazul/',
+        component: ComponentCreator('/grupo2/sprint1/Entendimento do negócio/Matriz Oceano Aazul/', '49d'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Entendimento do negócio/TAM, SAM e SOM/',
+        component: ComponentCreator('/grupo2/sprint1/Entendimento do negócio/TAM, SAM e SOM/', '088'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Processamento de requisições de peças/',
+        component: ComponentCreator('/grupo2/sprint1/Processamento de requisições de peças/', '599'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint1/Requisitos funcionais e não funcionais/',
+        component: ComponentCreator('/grupo2/sprint1/Requisitos funcionais e não funcionais/', '6a2'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint2/Frontend/',
+        component: ComponentCreator('/grupo2/sprint2/Frontend/', 'bcf'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint2/Funcionalidades do sistema/',
+        component: ComponentCreator('/grupo2/sprint2/Funcionalidades do sistema/', 'e07'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint2/Mapeamento/',
+        component: ComponentCreator('/grupo2/sprint2/Mapeamento/', '925'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint2/Setup do robô/',
+        component: ComponentCreator('/grupo2/sprint2/Setup do robô/', 'ca9'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint2/Testes do robô/',
+        component: ComponentCreator('/grupo2/sprint2/Testes do robô/', '1f2'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint3/Acompanhamento de requisitos/',
+        component: ComponentCreator('/grupo2/sprint3/Acompanhamento de requisitos/', '71d'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint3/Descrição de testes/',
+        component: ComponentCreator('/grupo2/sprint3/Descrição de testes/', 'f3c'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint3/Fluxo do chatbot/',
+        component: ComponentCreator('/grupo2/sprint3/Fluxo do chatbot/', 'f38'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint3/Funcionalidades do sistema/',
+        component: ComponentCreator('/grupo2/sprint3/Funcionalidades do sistema/', '9d7'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint3/LLM/',
+        component: ComponentCreator('/grupo2/sprint3/LLM/', '390'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint3/Modo de execução do projeto/',
+        component: ComponentCreator('/grupo2/sprint3/Modo de execução do projeto/', 'da6'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint3/Sistema de logs/',
+        component: ComponentCreator('/grupo2/sprint3/Sistema de logs/', '456'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint3/Sistema de navegação/',
+        component: ComponentCreator('/grupo2/sprint3/Sistema de navegação/', '9d9'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint4/Acompanhamento de requisitos/',
+        component: ComponentCreator('/grupo2/sprint4/Acompanhamento de requisitos/', '223'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint4/Funcionalidades Desenvolvidas/',
+        component: ComponentCreator('/grupo2/sprint4/Funcionalidades Desenvolvidas/', '3a5'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint4/Modo de execução do projeto/',
+        component: ComponentCreator('/grupo2/sprint4/Modo de execução do projeto/', '39f'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint4/Speech-to-Text/',
+        component: ComponentCreator('/grupo2/sprint4/Speech-to-Text/', '20e'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint4/Testes de usabilidade/',
+        component: ComponentCreator('/grupo2/sprint4/Testes de usabilidade/', '577'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint4/Text-to-Speech/',
+        component: ComponentCreator('/grupo2/sprint4/Text-to-Speech/', 'b23'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Arquitetura da solução/',
+        component: ComponentCreator('/grupo2/sprint5/Arquitetura da solução/', '7aa'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Eficiência do sistema/',
+        component: ComponentCreator('/grupo2/sprint5/Eficiência do sistema/', 'f85'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Entendimento da experiência do usuário/',
+        component: ComponentCreator('/grupo2/sprint5/Entendimento da experiência do usuário/', '8d4'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Entendimento do negócio/Análise Financeira/',
+        component: ComponentCreator('/grupo2/sprint5/Entendimento do negócio/Análise Financeira/', 'fc5'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Entendimento do negócio/Canvas Proposta de Valor/',
+        component: ComponentCreator('/grupo2/sprint5/Entendimento do negócio/Canvas Proposta de Valor/', '2d2'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Entendimento do negócio/Matriz de Risco/',
+        component: ComponentCreator('/grupo2/sprint5/Entendimento do negócio/Matriz de Risco/', '567'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Entendimento do negócio/Matriz Oceano Aazul/',
+        component: ComponentCreator('/grupo2/sprint5/Entendimento do negócio/Matriz Oceano Aazul/', '1b6'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Entendimento do negócio/TAM, SAM e SOM/',
+        component: ComponentCreator('/grupo2/sprint5/Entendimento do negócio/TAM, SAM e SOM/', '46f'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Interface do Usuário/',
+        component: ComponentCreator('/grupo2/sprint5/Interface do Usuário/', '617'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Roadmap/',
+        component: ComponentCreator('/grupo2/sprint5/Roadmap/', 'aff'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Sistema de cartografia/',
+        component: ComponentCreator('/grupo2/sprint5/Sistema de cartografia/', '27a'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Sistema de Chatbot/',
+        component: ComponentCreator('/grupo2/sprint5/Sistema de Chatbot/', '572'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Sistema de localização e mapeamento simultâneo/',
+        component: ComponentCreator('/grupo2/sprint5/Sistema de localização e mapeamento simultâneo/', '3aa'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Sistema de text-to-speech/',
+        component: ComponentCreator('/grupo2/sprint5/Sistema de text-to-speech/', '0c2'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/grupo2/sprint5/Speech-to-Text/',
+        component: ComponentCreator('/grupo2/sprint5/Speech-to-Text/', '869'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
